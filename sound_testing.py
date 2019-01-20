ec28ba2cd3ad4869b41b67fe101f95c62f2e07b6
Tim Seiger <tim.seiger+github@gmail.com>
2019-01-20 14:29:07 -0500

    added the next few chapters projects

---
+++ sound_testing.py
@@ -0,0 +1,40 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption("Hellow World!")
+
+# set up the colors
+BLACK = ( 0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+fontObj = pygame.font.Font('freesansbold.ttf', 32)
+textSurfaceObj = fontObj.render('Hello world!', True, GREEN, BLUE)
+textRectObj = textSurfaceObj.get_rect()
+textRectObj.center = (200, 150)
+
+loopcount = 0
+while True:
+    DISPLAYSURF.fill(WHITE)
+    DISPLAYSURF.blit(textSurfaceObj, textRectObj)
+    
+    soundObj = pygame.mixer.Sound("download/match0.wav")
+    if loopcount == 0:
+        soundObj.play()
+        import time
+        time.sleep(1)
+        soundObj.stop()
+    
+    loopcount += 1
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    pygame.display.update()
+
